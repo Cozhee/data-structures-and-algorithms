878ea1160f5dff1c92e47dcc2992e0e9719a6ded
Cody Davis <davis.r.cody@gmail.com>
2022-06-26 20:26:14 -0700

    challenge 05

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,18 @@
+## Linked List Implementations
+
+-Create a linked list
+-Create a node
+
+## addToFront
+- Should be able to add a node to the head of the linked list
+
+## addToEnd
+- Should be able to add a node to the end of the linked list
+
+## includes
+- Check if a value is present in the linked list
+
+## traverse
+- Prints out the values in the linked list in a `{ val } => { val } => { val } => NULL` format
+
+
--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,34 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const linkedList = require('../index');
 
 describe('Linked List', () => {
   it('works', () => {
-    expect(true).toBeTruthy();
+
+    linkedList.addToFront(6)
+    expect(linkedList.head.value).toEqual(6);
+
   });
+
+  it('works', () => {
+
+    linkedList.addToEnd(4)
+    expect(linkedList.head.next.value).toEqual(4)
+
+  })
+
+  it('should print values', () => {
+
+    expect(linkedList.traverse()).toBe('{ 6 } -> { 4 } -> NULL')
+
+  })
+
+  it('Should return true', () => {
+
+    expect(linkedList.includes(6)).toBeTruthy()
+    expect(linkedList.includes(4)).toBeTruthy()
+    expect(linkedList.includes(8)).toBeFalsy()
+
+  })
 });
