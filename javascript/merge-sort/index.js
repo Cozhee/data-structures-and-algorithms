80f48846a94c9a19fbb311ccfb9b8cef16b81d0f
Cozhee <55909913+Cozhee@users.noreply.github.com>
2022-07-19 18:45:21 -0700

    Update index.js

--- javascript/merge-sort/index.js
+++ javascript/merge-sort/index.js
@@ -1,1 +1,40 @@
+const merge = (leftArr, rightArr) => {
+    const output = []
+    let leftIndex = 0
+    let rightIndex = 0
 
+    while(leftIndex < leftArr.length && rightIndex < rightArr.length) {
+        const left = leftArr[leftIndex]
+        const right = rightArr[rightIndex]
+
+        if(left < right) {
+            output.push(left)
+            leftIndex++
+        } else {
+            output.push(right)
+            rightIndex++
+        }
+    }
+
+    return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)]
+}
+
+const mergeSort = array => {
+    // base case
+    if(array.length <= 1) {
+        return array
+    }
+
+    const middle = Math.floor(array.length / 2)
+    const left = array.slice(0, middle)
+    const right = array.slice(middle)
+
+    return merge(
+        mergeSort(left), mergeSort(right)
+    )
+}
+
+
+const someArr = [3, 5, 2, 1, 32, 31, 65, 20, 13, 25, 15, 14, 8]
+
+console.log(mergeSort(someArr))
